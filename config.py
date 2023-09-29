da66ffb17d55620a11e6a212e4659b2d9cb0cdba
Luiz Antonio Calliari Filho <luizcalliari.filho@gmail.com>
2023-09-29 20:29:48 -0300

    Changing menu posution and adding taggle to floatting window.

--- config.py
+++ config.py
@@ -142,7 +142,6 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.Spacer(),
                 widget.Sep(),
                 widget.WidgetBox(
                     text_closed="Menu",
@@ -225,6 +224,7 @@ screens = [
                         ),
                     ],
                 ),
+                widget.Spacer(),
                 widget.Sep(),
                 widget.CPU(format="Cpu: {load_percent}%"),
                 widget.CPUGraph(),
@@ -292,6 +292,7 @@ mouse = [
         [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
     ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
+    Click([mod, "control"], "Button1", lazy.window.toggle_floating()),
 ]
 
 dgroups_key_binder = None
