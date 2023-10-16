9f368f6532ec7457e4817e6a862365c500a8ba2a
Luiz Antonio Calliari Filho <luizcalliari.filho@gmail.com>
2023-10-15 22:13:09 -0300

    separating config to avoid errors.

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-xrandr --output HDMI-1 --primary --mode 2560x1080
+xrandr --output HDMI-1 --primary
+xrandr --output HDMI-1 --mode 3840x2060
+xrandr --output HDMI-1 --mode 2560x1080
 xrandr --output HDMI-2 --rotate left --mode 1920x1080
 xset s off
 xset -dpms
