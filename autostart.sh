f4494691414752bc91061e6f8dca1e065214f540
Luiz Antonio Calliari Filho <luizcalliari.filho@gmail.com>
2023-10-15 21:51:43 -0300

    Updateing HDMI-1 config.

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-xrandr --output HDMI-1 --primary #--mode 1920x1080
+xrandr --output HDMI-1 --primary --mode 2560x1080
 xrandr --output HDMI-2 --rotate left --mode 1920x1080
 xset s off
 xset -dpms
