18cdbe4283f50d8758ad75b55d625267a6abda80
Luiz Antonio Calliari Filho <luizcalliari.filho@gmail.com>
2023-10-16 21:46:40 -0300

    Changing basic resolution.

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 xrandr --output HDMI-1 --primary
-xrandr --output HDMI-1 --mode 3840x2060
+xrandr --output HDMI-1 --mode 1920x1080
 xrandr --output HDMI-1 --mode 2560x1080
 xrandr --output HDMI-2 --rotate left --mode 1920x1080
 xset s off
