0a27346f75a9e9da9ec002fc2da52cea3e4e6bd3
Luiz Antonio Calliari Filho <luizcalliari.filho@gmail.com>
2024-01-09 11:24:13 -0300

    Corrigindo configs para o SSD.

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 xrandr --output HDMI-1 --primary
-xrandr --output HDMI-1 --mode 1920x1080
+#xrandr --output HDMI-1 --mode 1920x1080
 xrandr --output HDMI-1 --mode 2560x1080
-xrandr --output HDMI-2 --rotate left --mode 1920x1080
+#xrandr --output HDMI-2 --rotate left --mode 1920x1080
+xrandr --output HDMI-0 --rotate left --mode 1920x1080
 xset s off
 xset -dpms
