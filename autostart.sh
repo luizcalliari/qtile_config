efbd559dfca45428c58005b2ae96b0788683c487
LACF <luizcalliari.filho@gmail.com>
2026-04-05 10:11:47 -0300

    update autostart script with screen and keyboard configuration adjustments

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+
+#Screen configuration
 xrandr --output HDMI-1 --primary
 #xrandr --output HDMI-1 --mode 1920x1080
 xrandr --output HDMI-1 --mode 2560x1080
 #xrandr --output HDMI-2 --rotate left --mode 1920x1080
 xrandr --output HDMI-0 --rotate left --mode 1920x1080
+
+#This disables the software screensaver.
+# It prevents the screen from blanking out after a period of inactivity.
 xset s off
 xset -dpms
+
+#Configure keyboard
 setxkbmap -layout us -variant intl
 setxkbmap -layout us -variant altgr-intl
 
